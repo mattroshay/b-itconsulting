43376b3c359956996f0f3b375f5a5db91f34310e
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2025-05-19 16:50:02 +0200

    fixed routing, removed login require for about, competences & contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :contact, :competences ]
 
   def home
   end
   def about
   end
   def contact
+  end
+  def competences
   end
 end
