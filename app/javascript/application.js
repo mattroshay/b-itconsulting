c5c20d46a71fb8a8f0eff84cc43f37e03a456228
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2025-05-22 15:53:08 +0200

    added scroll to top button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,34 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+//scrolltop-button
+document.addEventListener("turbo:load", function () {
+  var scrollTop = document.getElementById("scrollTop");
+
+  if (!scrollTop) return; // safeguard in case the button doesn't exist on the page
+
+  window.onscroll = function () {
+    scrollfunction();
+  };
+
+  function scrollfunction() {
+    if (
+      document.body.scrollTop > 95 ||
+      document.documentElement.scrollTop > 95
+    ) {
+      scrollTop.style.display = "block";
+    } else {
+      scrollTop.style.display = "none";
+    }
+  }
+
+  scrollTop.addEventListener("click", function () {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
+//scrolltop-button end
