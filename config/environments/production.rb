e281da051115a476eed03eaee9e23acb065c891c
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2025-10-03 11:34:44 +0200

    updated production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,11 +100,12 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
-    port: ENV["SMTP_PORT"],
+    port: ENV.fetch("SMTP_PORT", "465").to_i,
     domain: ENV["SMTP_DOMAIN"],
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
     authentication: :plain,
-    enable_starttls_auto: true
+    ssl: true,
+    enable_starttls_auto: false
   }
 end
